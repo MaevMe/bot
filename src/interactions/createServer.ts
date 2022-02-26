e77a384be3cc3470fa16119b760bbcf5bf6095c8
Norah Maria Hareide Nilsen <norahmaria@Norahs-MBP.domain_not_set.invalid>
2022-02-26 08:23:12 +0100

    Create Server on guildCreate

--- src/interactions/createServer.ts
+++ src/interactions/createServer.ts
@@ -4,9 +4,16 @@ import Server from '../models/Server'
 const interaction: Interaction = {
   guildCreate: async (client, guild) => {
     try {
-      await Server.create({
+      const server = await Server.create({
         id: guild.id,
+        tempVoiceChannels: {
+          active: false,
+          createChannel: '',
+          namingFormat: '',
+          categoryID: '',
+        },
       })
+      console.log('@createServer success', server)
     } catch (err) {
       console.error('@createServer', err)
     }
